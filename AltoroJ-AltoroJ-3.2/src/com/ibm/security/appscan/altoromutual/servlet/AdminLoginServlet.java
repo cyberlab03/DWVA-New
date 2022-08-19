8743569859d3394cd1e2e9629cdabccc8f2c9ea1
nmishr1 <101803837+nmishr1@users.noreply.github.com>
2022-08-19 06:50:22 -0700

    Update AdminLoginServlet.java

--- AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/servlet/AdminLoginServlet.java
+++ AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/servlet/AdminLoginServlet.java
@@ -42,7 +42,7 @@ public class AdminLoginServlet extends HttpServlet {
 		if (password == null){
 			response.sendRedirect(request.getContextPath()+"/admin/login.jsp");
 			return ;
-		} else if (!password.equals("Altoro mutual123")){
+		} else if (!password.equals("Altoro mutual0000")){
 			request.setAttribute("loginError", "Login failed.");
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/login.jsp");
 			dispatcher.forward(request, response);
